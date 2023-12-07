ba0f0aa80eb3bc84eaa7c31752a4e843ac72ddd4
Tin Duong <114547634+Tin-Duong1@users.noreply.github.com>
2023-12-06 22:35:35 -0600

    React changes basicaly the whole thing and back end changes slightly
    changed basically the whole project hopes this doesnt brick

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import './App.css';
 
-
-import {
-  BrowserRouter as Router, Route, Routes
-} from "react-router-dom"
-
-import AddNote from './Pages/AddNote';
-import Delete from './Pages/Delete';
-import Read from './Pages/Read';
-import Login from './Pages/login';
-import Register from './Pages/register';
-
+import NoteListPage from './Pages/NoteListPage';
+import AddNotePage from './Pages/AddNotePage';
+import EditNotePage from './Pages/EditNotePage';
 import Header from './Components/Header';
 
 function App() {
-  
   return (
     <Router>
       <Header />
       <div>
-      <Routes>
-        <Route exact path="/" element={
-          <>
-          <Read />
-          </>
-        }></Route>
-        <Route path='/delete' element={<Delete />}></Route>
-        <Route path='/write' element={<AddNote />}></Route>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/register' element={<Register />}></Route>
-      </Routes>
+        <Routes>
+          <Route exact path="/" element={<NoteListPage />} />
+          <Route path="/write" element={<AddNotePage />} />
+          <Route path="/edit/:noteId" element={<EditNotePage />} />
+        </Routes>
       </div>
-      </Router>
-    
-  )
+    </Router>
+  );
 }
 
 
+export default App;
 
-export default App
---
+++ client/src/Pages/NotesList.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import NoteItem from './NoteItem';
+import './NoteListPage.css';
+
+function NoteListPage() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/notes')
+      .then(response => response.json())
+      .then(data => setNotes(data))
+      .catch(error => console.error('Error fetching notes:', error));
+  }, []);
+
+  return (
+    <div className="note-list-page">
+      <h2>All Notes</h2>
+      <div className="notes-container">
+        {notes.map(note => <NoteItem key={note.id} note={note} />)}
+      </div>
+    </div>
+  );
+}
+
+export default NoteListPage;
+
